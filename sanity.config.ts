e91717f3c54c40f523ae444b91c5cfee50f90bd4
codegigs-web <codegigs.web@gmail.com>
2024-02-04 20:11:45 +0530

    first commit

--- sanity.config.ts
+++ sanity.config.ts
@@ -5,6 +5,8 @@
 import {visionTool} from '@sanity/vision'
 import {defineConfig} from 'sanity'
 import {structureTool} from 'sanity/structure'
+import { unsplashImageAsset } from 'sanity-plugin-asset-source-unsplash'
+import { codeInput } from "@sanity/code-input";
 
 // Go to https://www.sanity.io/docs/api-versioning to learn how API versioning works
 import {apiVersion, dataset, projectId} from './src/sanity/env'
@@ -18,8 +20,10 @@ export default defineConfig({
   schema,
   plugins: [
     structureTool(),
+    unsplashImageAsset(),
     // Vision is a tool that lets you query your content with GROQ in the studio
     // https://www.sanity.io/docs/the-vision-plugin
     visionTool({defaultApiVersion: apiVersion}),
+    codeInput()
   ],
 })
--- src/sanity/schemas/tag.ts
+++ src/sanity/schemas/tag.ts
@@ -6,6 +6,21 @@ export default defineType({
   type: 'document',
   fields: [
     defineField({
+      name: 'mainImage',
+      title: 'Main image',
+      type: 'image',
+      options: {
+        hotspot: true,
+      },
+      fields: [
+        {
+          name: 'alt',
+          type: 'string',
+          title: 'Alternative Text',
+        }
+      ]
+    }),
+    defineField({
       name: 'title',
       title: 'Title',
       type: 'string',
